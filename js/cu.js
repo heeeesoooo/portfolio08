bceb3e873ae06d3fb121a841bbe92014fb8097c8
heeeesoooo <118698717+heeeesoooo@users.noreply.github.com>
2023-01-27 17:47:02 +0900

    Add files via upload

---
+++ js/cu.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.main_slide').slick({
+        slidesToShow: 3,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    })
+})
